719442711ca80a549da2df7679bb3f165eb4a75a
Михаил Пигалов <pigalovmihail@yandex.ru>
2023-06-02 12:43:03 +0300

    Праvки 02.06.2023

--- Messenger/Pages/FriendsPage.xaml.cs
+++ Messenger/Pages/FriendsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Messenger.Entites;
+using Messenger.Windows;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -23,7 +24,59 @@ namespace Messenger.Pages
     public partial class FriendsPage : Page
     {
 
-        public static ObservableCollection<Messenger_User> friends = new ObservableCollection<Messenger_User>(App.Context.Messenger_User.Where(x=>x.Id!=App.CurrentUser.Id).OrderBy(x=>x.Nickname).ToList());
+        public static ObservableCollection<Messenger_User> friends = null;
+
+        void RefreshData()
+        {
+
+            var requests = App.Context.Messenger_RequestOfFriendship.Where(x=>x.SendeUserrId==App.CurrentUser.Id).ToList();
+
+            var _requests = App.Context.Messenger_RequestOfFriendship.Where(x=>x.RecieverUserId==App.CurrentUser.Id).ToList();
+
+            List<int> ids = new List<int>();
+
+            List<int> idsTo = new List<int>();
+
+            List<int> idsFriend = new List<int>();
+
+            foreach (var request in requests)
+            {
+                ids.Add(request.RecieverUserId);
+            }
+
+            foreach(var _request in _requests)
+            {
+                idsTo.Add(_request.SendeUserrId);
+            }
+
+            foreach(var friend in App.Context.Messenger_Friendship.Where(x => x.IdFirstUser == App.CurrentUser.Id || x.IdSecondUser==App.CurrentUser.Id).ToList())
+            {
+                if (friend.IdFirstUser == App.CurrentUser.Id)
+                {
+                    idsFriend.Add(friend.IdSecondUser);
+                }
+                else
+                {
+                    idsFriend.Add(friend.IdFirstUser);
+                }
+            }
+
+            friends = new ObservableCollection<Messenger_User>(App.Context.Messenger_User.Where(x => x.Id != App.CurrentUser.Id
+            && !ids.Contains(x.Id) 
+            && !idsTo.Contains(x.Id)
+            && !idsFriend.Contains(x.Id)).OrderBy(x => x.Nickname));
+
+            CountOfUserTextBlock.Text = $"Пользователи ({friends.Count})";
+
+            FriendsListBox.ItemsSource = friends;
+
+            RequestsFromMeListBox.ItemsSource = App.Context.Messenger_User.Where(x => ids.Contains(x.Id)).ToList();
+
+            RequestsToMeListBox.ItemsSource = App.Context.Messenger_User.Where(x => idsTo.Contains(x.Id)).ToList();
+
+            FriendsListView.ItemsSource = App.Context.Messenger_User.Where(x=> idsFriend.Contains(x.Id)).ToList();
+
+        }
 
         public FriendsPage()
         {
@@ -31,17 +84,13 @@ namespace Messenger.Pages
 
             App.AnimateBorderOpacity(this);
 
-            FriendsListBox.ItemsSource = friends;
-
-            RequestsFromMeListBox.ItemsSource = friends;
-
-            RequestsToMeListBox.ItemsSource = friends;
+            RefreshData();
         }
 
 
         private void SearchTextBoxTextChanged(object sender, TextChangedEventArgs e)
         {
-            FriendsListBox.ItemsSource = friends.Where(x=>x.Nickname.Contains(SearchTextBox.Text));
+            FriendsListBox.ItemsSource = friends.Where(x => x.Nickname.Contains(SearchTextBox.Text));
         }
 
         private void AddFriendButtonClick(object sender, RoutedEventArgs e)
@@ -49,16 +98,153 @@ namespace Messenger.Pages
 
             var friend = (sender as Button).DataContext as Messenger_User;
 
-            if(friend != null) 
+            if (friend != null)
             {
-                //Не забыть обновить БД и свойства скопировать
-                //создаем заявку и меня текст кнопки на "заявка отправлена"
+                try
+                {
+                    App.Context.Messenger_RequestOfFriendship.Add(new Messenger_RequestOfFriendship
+                    {
+                        SendeUserrId = App.CurrentUser.Id,
+                        RecieverUserId = friend.Id,
+                    });
+                    App.Context.SaveChanges();
+
+                    RefreshData();
+                }
+                catch (Exception ex)
+                {
+                    new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", ex.Message).ShowDialog();
+                }
+
             }
             else
             {
-                //выдаем ошибку
+                new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", "Пользователь = null").ShowDialog();
             }
 
+        }
+
+        private void AgreeAddFriendButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            var SelectedUser = ((sender as Button).DataContext as Messenger_User).Id;
+
+            var request = App.Context.Messenger_RequestOfFriendship
+                .Where(x => x.SendeUserrId == SelectedUser && x.RecieverUserId == App.CurrentUser.Id)
+                .FirstOrDefault();
+
+            if (request != null)
+            {
+                try
+                {
+                    App.Context.Messenger_Friendship.Add(new Messenger_Friendship
+                    {
+                        IdFirstUser = App.CurrentUser.Id,
+                        IdSecondUser = request.SendeUserrId,
+                        DateTimeOfFriendship = DateTime.Now,
+                    });
+                    App.Context.Messenger_RequestOfFriendship.Remove(request);
+                    App.Context.SaveChanges();
+
+                    RefreshData();
+                }
+                catch (Exception ex)
+                {
+                    new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", ex.Message).ShowDialog();
+                }
+
+            }
+            else
+            {
+                new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", "Запрос на дружбу = null").ShowDialog();
+            }
+        }
+
+        private void DisagreeAddFriendButtonClick(object sender, RoutedEventArgs e)
+        {
+            var SelectedUser = ((sender as Button).DataContext as Messenger_User).Id;
+
+            var request = App.Context.Messenger_RequestOfFriendship
+                .Where(x => x.SendeUserrId == SelectedUser && x.RecieverUserId == App.CurrentUser.Id)
+                .FirstOrDefault();
+
+            if (request != null)
+            {
+                try
+                {
+                    App.Context.Messenger_RequestOfFriendship.Remove(request);
+                    App.Context.SaveChanges();
+
+                    RefreshData();
+                }
+                catch (Exception ex)
+                {
+                    new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", ex.Message).ShowDialog();
+                }
+
+            }
+            else
+            {
+                new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", "Запрос на дружбу = null").ShowDialog();
+            }
+        }
+
+        private void UndoRequestButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            var SelectedUser = ((sender as Button).DataContext as Messenger_User).Id;
+
+            var request = App.Context.Messenger_RequestOfFriendship
+                .Where(x => x.RecieverUserId == SelectedUser && x.SendeUserrId == App.CurrentUser.Id)
+                .FirstOrDefault();
+
+            if (request != null)
+            {
+                try
+                {
+                    App.Context.Messenger_RequestOfFriendship.Remove(request);
+                    App.Context.SaveChanges();
+
+                    RefreshData();
+                }
+                catch (Exception ex)
+                {
+                    new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", ex.Message).ShowDialog();
+                }
+
+            }
+            else
+            {
+                new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", "Запрос на дружбу = null").ShowDialog();
+            }
+        }
+
+        private void BorderMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            if(MyFriendsGrid.Visibility == Visibility.Visible)
+            {
+                MyFriendsGrid.Visibility = Visibility.Hidden;
+                SearchFriendsGrid.Visibility = Visibility.Visible;
+                App.AnimateBorderOpacity(MyFriendsGrid);
+                ButtonText.Text = "Список друзей";
+            }
+            else
+            {
+                MyFriendsGrid.Visibility = Visibility.Visible;
+                SearchFriendsGrid.Visibility = Visibility.Hidden;
+                App.AnimateBorderOpacity(SearchFriendsGrid);
+                ButtonText.Text = "Поиск друзей";
+            }
+
+        }
+
+        private void WriteMessageTextMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            var user = (sender as TextBlock).DataContext as Messenger_User;
+
+            new WriteWindow(Window.GetWindow(this), user).ShowDialog();
         }
     }
 
---
+++ Messenger/Windows/WriteWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Messenger.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Messenger.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WriteWindow.xaml
+    /// </summary>
+    public partial class WriteWindow : Window
+    {
+
+        Messenger_User Reciever = new Messenger_User();
+
+        public WriteWindow(Window Parent, Messenger_User _Reciever)
+        {
+            InitializeComponent();
+            this.Owner = Parent;
+            Reciever = _Reciever;
+            NicknameTextBlock.Text = Reciever.Nickname;
+        }
+
+        private void SendMessageButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = App.Context.Messenger_Dialog.Where(x=> (x.IdFirstUser==App.CurrentUser.Id && x.IdSecondUser==Reciever.Id) || (x.IdFirstUser == Reciever.Id && x.IdSecondUser == App.CurrentUser.Id)).FirstOrDefault();
+
+            if (MessageTextBox.Text != "")
+            {
+                if (dialog != null)
+                {
+                    App.Context.Messenger_Message.Add(new Messenger_Message
+                    {
+                        Message = MessageTextBox.Text,
+                        IdDialog = dialog.Id,
+                        Time = DateTime.Now,
+                        LastIdSenderUser = App.CurrentUser.Id,
+                        LastNicknameSenderUser = App.CurrentUser.Nickname,
+                    });
+                    dialog.LastMessage = MessageTextBox.Text;
+                    App.Context.SaveChanges();
+                }
+                else
+                {
+                    Messenger_Dialog newDialog = new Messenger_Dialog()
+                    {
+                        IdFirstUser = App.CurrentUser.Id,
+                        IdSecondUser = Reciever.Id,
+                        LastMessage = ""
+                    };
+
+                    App.Context.Messenger_Dialog.Add(newDialog);
+
+                    App.Context.SaveChanges();
+
+                    App.Context.Messenger_Message.Add(new Messenger_Message
+                    {
+                        Message = MessageTextBox.Text,
+                        IdDialog = newDialog.Id,
+                        Time = DateTime.Now,
+                        LastIdSenderUser = App.CurrentUser.Id,
+                        LastNicknameSenderUser = App.CurrentUser.Nickname,
+                    });
+
+                    newDialog.LastMessage = MessageTextBox.Text;
+
+                    App.Context.SaveChanges();
+
+                    new Windows.NewMessageBox(Window.GetWindow(this), "Успех", "Сообщение отправлено!").ShowDialog();
+
+                    this.Close();
+                }
+            }
+            else
+            {
+                new Windows.NewMessageBox(Window.GetWindow(this), "Ошибка", "Сообщение не может быть пустым!").ShowDialog();
+            }
+
+        }
+    }
+}
